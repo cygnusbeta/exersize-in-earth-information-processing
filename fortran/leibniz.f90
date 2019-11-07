98a00c29d0a0635427e92d7583363b57c1f27733
cygnusbeta <betacygnib+git@gmail.com>
2019-11-07 11:55:14 +0900

    Change type

--- fortran/leibniz.f90
+++ fortran/leibniz.f90
@@ -1,6 +1,7 @@
 program test
     implicit none
-    real :: k, sum = 0, n = 10 ** 8, pi
+    integer, parameter :: n = 10 ** 8
+    real :: k, pi, sum = 0
     do k = 0, n
         sum = sum + (-1) ** k / (2 * k + 1)
     end do
