b00f645f4b20018a4bd0b555322c0a7f582b6264
cygnusbeta <betacygnib+git@gmail.com>
2020-01-27 12:02:11 +0900

    Add pca

--- r/quake/quake.R
+++ r/quake/quake.R
@@ -1,6 +1,6 @@
 x <- read.csv('quake/query.csv')
 x <- data.frame(x$longitude, x$depth, x$mag)
-colnames(x) <- c("longitude", "depth", "mag")
+colnames(x) <- c('longitude', 'depth', 'mag')
 # print(x)
 means <- apply(x, 2, mean)
 print('means')
@@ -13,3 +13,21 @@ print(stds)
 plot(x$longitude, x$depth)
 plot(x$longitude, x$mag)
 plot(x$depth, x$mag)
+
+print('correlation matrix')
+cor(cbind(x))
+
+print('--- pca ---')
+pca <- prcomp(x, scale=TRUE)
+# print(pca)
+
+print('固有値(の正の平方根)')
+print(pca$sdev)
+
+print('固有ベクトル')
+print(pca$rotation)
+
+print('主成分得点')
+print(pca$x)
+
+biplot(pca)
